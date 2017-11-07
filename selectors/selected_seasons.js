385297de702396d4686a386c95531c74aad1cbe2
John Baek <jbaek7023@gmail.com>
2017-11-08 02:33:49 +0900

    TouchableOpactiy Error was due to Development mode

--- selectors/selected_seasons.js
+++ selectors/selected_seasons.js
@@ -5,7 +5,13 @@ import _ from 'lodash';
 const seasonsSelector = state => state.seasons
 const selectedSeasonsSelector = state => state.selectedSeasonIds
 
+// seasons  = {30: 'Fall', 31: 'Spring', 32: 'All'}
+// selectedSeasonIds = {30: true, 31: false, 32: true}
+
+// selectedSeasons = {30: 'Fall', 32: 'All'}
+
 const getSeasons = (seasons, selectedSeasonIds) => {
+
   const selectedSeasons = _.filter(
     seasons,
     season => _.includes(selectedSeasonIds, season.id)
--- utils/menuItems.js
+++ utils/menuItems.js
@@ -1,3 +1,4 @@
+
 // 1
 const bigType = [{
   id: 1,
