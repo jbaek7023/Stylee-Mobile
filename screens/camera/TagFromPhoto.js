c755b121fb8fb6dbf02222f43add04d93b53f546
John Baek <jbaek7023@gmail.com>
2017-11-15 22:02:35 +0900

    Can Move Selected Square

--- screens/camera/TagFromPhoto.js
+++ screens/camera/TagFromPhoto.js
@@ -4,12 +4,141 @@ import { width, height } from 'react-native-dimension';
 import { RkStyleSheet, RkText } from 'react-native-ui-kitten';
 import { Ionicons } from '@expo/vector-icons';
 import {FontAwesome} from '../../assets/icons';
+import {createResponder} from 'react-native-gesture-responder';
+import _ from 'lodash';
 
 class TagFromPhoto extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Tag From Photo',
     tabBarVisible: false
   });
+
+  state = {
+    gestureState: {},
+    thumbSize: 100,
+    left: width(100) / 2,
+    top: height(100) / 2,
+    taggedClothes: {
+      0: {id:0, left:100, top:100, thumbSize:100},
+      1: {id:1, left:200, top:200, thumbSize:200},
+    },
+    selectedClothId : 0,
+  }
+
+  componentWillMount() {
+    this.gestureResponder = createResponder({
+      onStartShouldSetResponder: (evt, gestureState) => true,
+      onStartShouldSetResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetResponder: (evt, gestureState) => true,
+      onMoveShouldSetResponderCapture: (evt, gestureState) => true,
+      onResponderGrant: (evt, gestureState) => {
+
+      },
+      onResponderMove: (evt, gestureState) => {
+        let { taggedClothes } = this.state;
+        let tag = taggedClothes[this.state.selectedClothId];
+
+        // pinch
+        let thumbSize = tag.thumbSize;
+        if (gestureState.pinch && gestureState.previousPinch) {
+          thumbSize *= (gestureState.pinch / gestureState.previousPinch)
+        }
+        let {left, top} = tag;
+        left += (gestureState.moveX - gestureState.previousMoveX);
+        top += (gestureState.moveY - gestureState.previousMoveY);
+
+        let topbby = top - thumbSize/2;
+        let lefty = left - thumbSize/2;
+        let righty = left + thumbSize/2;
+        let bottomy = top + thumbSize/2;
+
+        if (topbby>=0 && bottomy <= width(100) && righty <= width(100) && lefty >= 0) {
+
+          var deep = _.cloneDeep(tag);
+          deep.left = left
+          deep.top = top
+          deep.thumbSize = thumbSize
+          this.setState({
+            gestureState: {
+              ...gestureState
+            },
+            taggedClothes: {
+              ...taggedClothes,
+              [this.state.selectedClothId]: deep
+            }
+          })
+        } else {
+          console.log('do nothing')
+        }
+      },
+      onResponderTerminationRequest: (evt, gestureState) => true,
+      onResponderRelease: (evt, gestureState) => {
+        this.setState({
+          gestureState: {
+            ...gestureState
+          }
+        })
+      },
+      onResponderTerminate: (evt, gestureState) => {},
+
+      onResponderSingleTapConfirmed: (evt, gestureState) => {
+        // console.log(gestureState.x0, gestureState.y0);
+        // // if x0, y0 in selected?
+        //   // select the item.
+        // //
+        // console.log(gestureState);
+      },
+      debug: false
+    });
+  }
+
+  _renderTags = () => {
+    let { taggedClothes } = this.state;
+    taggedItems = Object.keys(taggedClothes);
+    if(taggedItems.length==0) {
+      return <View />
+    }
+
+    let output = taggedItems.map((key) => {
+      let cloth = taggedClothes[key];
+      let thumbSize = cloth.thumbSize;
+      let top = cloth.top - thumbSize/2;
+      let left = cloth.left - thumbSize/2;
+      console.log('print--------');
+      console.log(key);
+      console.log(this.state.selectedClothId);
+      if(key==this.state.selectedClothId) {
+        return (
+          <View
+            key={key}
+            style={{
+              top,
+              left,
+              height: thumbSize,
+              width: thumbSize,
+              borderWidth: 2,
+              borderColor: 'yellow'
+            }}
+          />
+        );
+      } else {
+        return (
+          <View
+            key={key}
+            style={{
+              top,
+              left,
+              height: thumbSize,
+              width: thumbSize,
+              borderWidth: 2,
+              borderColor: 'white'
+            }}
+          />
+        );
+      }
+    });
+    return output;
+  }
 
   _renderTaggedClothImage = () => {
     return (
@@ -22,6 +151,7 @@ class TagFromPhoto extends Component {
   }
 
   _renderPhotoImage = (image) => {
+    const thumbSize = this.state.thumbSize;
     return (
       <ImageBackground
         source={{uri: image}}
@@ -30,35 +160,37 @@ class TagFromPhoto extends Component {
         <View
           style={{
             position: 'absolute',
-            top: 120,
-            left: 100,
-            height: 50,
-            width: 50,
+            top: this.state.top - thumbSize/2,
+            left: this.state.left - thumbSize/2,
+            height: thumbSize,
+            width: thumbSize,
             borderWidth: 1,
             borderColor: 'white'
           }}
+          pointerEvents='none'
         />
-        <View
-          style={{
-            position: 'absolute',
-            top: 100,
-            left: 150,
-            height: 200,
-            width: 200,
-            borderWidth: 1,
-            borderColor: 'white'
-          }}
-        />
+        <View style={styles.imageStyle}>{this._renderTags()}</View>
       </ImageBackground>
     );
   }
 
   render() {
     let { image } = this.props.navigation.state.params;
+    let { thumbSize } = this.state;
+    let top = this.state.top - thumbSize/2;
+    let left = this.state.left - thumbSize/2;
+    let right = this.state.left + thumbSize/2;
+    let bottom = this.state.top + thumbSize/2;
+
     if(image) {
       return (
         <ScrollView>
+          <View
+            style={styles.imageStyle}
+            {... this.gestureResponder}
+          >
           {this._renderPhotoImage(image)}
+          </View>
           <View style={styles.dContainer}>
             <RkText rkType="header4">Tagged Clothes (1)</RkText>
           </View>
@@ -92,7 +224,6 @@ class TagFromPhoto extends Component {
         <View><Text>This is not supposed to happen (You can send this image to admin)</Text></View>
       );
     }
-
   }
 }
 
